4a0279e53bcf93acc8cf6a635136ef9da5e52186
alincqeek <pstartceva@yandex.ru>
2026-04-01 11:34:48 +0300

    promises

--- js/main.js
+++ js/main.js
@@ -138,7 +138,74 @@
 // console.log(Utils.greet("Мария"));
 // console.log("Умножение: ",Utils.multiply(3 ,9));
 
-import * as Utils from "./math.js";
-console.log("4√7=",Utils.square(4,2));
-console.log("4**7=",Utils.cube(4,2));
-console.log("Значение E=",Utils.E );
+// import * as Utils from "./math.js";
+// console.log("4√7=",Utils.square(4,2));
+// console.log("4**7=",Utils.cube(4,2));
+// console.log("Значение E=",Utils.E );
+
+// console.log("Промисы");
+// const simplePromise=new Promise((resolve,reject)=>{
+//     const success=true;
+//     if (success){
+//         resolve ("Операция выполнена успешно!") ;
+//     }else{
+//         reject("Произошла ошибка") ;
+//     }
+// })
+// simplePromise
+//     .then((result) => console.log("Результат:",result))
+//     .catch((error) => console.log("Ошибка:", error));
+
+// function delay(ms){
+//     return new Promise((resolve) => {
+//         setTimeout(()=>{
+//             resolve(`Прошло ${ms} миллисекунд`);
+//         },ms);
+//     })
+// }
+// delay(1000)
+//     .then((message)=>console.log(message));
+
+// function fetchUserData(userId){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(() => {
+//             if (userId >0){
+//                 resolve({
+//                    id:userId,
+//                    name:"Иван Иванович",
+//                    email:"ivan@example.com" ,
+//                 });
+//             }else{
+//                 reject("Неверный ID пользователя")
+//             }
+//         }, 1500);
+//     })
+// }
+// fetchUserData(1)
+//     .then((user)=>console.log(user))
+//     .catch((error)=>console.log(error));
+
+// function step1() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve("Шаг 1 завершён"), 500);
+//   });
+// }
+
+// function step2(previousResult) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve(`${previousResult} -> Шаг 2 завершён`), 500);
+//   });
+// }
+
+// function step3(previousResult) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve(`${previousResult} -> Шаг 3 завершён`), 500);
+//   });
+// }
+
+
+// step1()
+//     .then((result1)=>step2(result1))
+//     .then((result2)=>step3(result2))
+//     .then((finalResult)=>consol.log("Финальный результат:", finalResult))
+//     .catch((error)=>console.log("Ошибка",error))
